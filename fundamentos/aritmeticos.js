64278229da6b47057959d9d1b8023ca73567f3da
Samuel gonçalves <goncalvesamuel99@gmail.com>
2021-06-16 11:30:58 +0100

    fundamentos.js

---
+++ fundamentos/aritmeticos.js
@@ -0,0 +1,11 @@
+const [a, b, c, d] = [3, 5, 1, 15];
+
+//nestes consts são utilizados opeardores binários
+const soma = a + b + c + d;
+const subtracao = d - b;
+const multiplicacao = a * b;
+const divisao = d / a;
+const modulo = a % 2;
+
+//neste console log, a divisao tem o operador unario -, ou seja vai imprimir o numero negativo
+console.log(soma, subtracao, multiplicacao, -divisao, modulo);
