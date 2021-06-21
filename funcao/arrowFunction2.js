bef428c08aa35db627e982ba4c9623a1e910421d
Samuel gonçalves <goncalvesamuel99@gmail.com>
2021-06-21 10:45:06 +0100

    funcao,js

--- funcao/arrowFunction2.js
+++ funcao/arrowFunction2.js
@@ -1,10 +1,11 @@
+//o mesmo exemplo que o ficheiro thisBind2.js mas neste foi utilizado arrow function
 function Pessoa() {
-    this.idade = 0
+  this.idade = 0;
 
-    setInterval(() => {
-        this.idade++
-        console.log(this.idade)
-    }, 1000)
+  setInterval(() => {
+    this.idade++;
+    console.log(this.idade);
+  }, 1000);
 }
 
-new Pessoa
+new Pessoa();
