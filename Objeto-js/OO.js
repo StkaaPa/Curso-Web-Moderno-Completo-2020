a8a62bd323240d2f9064bc4ea916a52ffe3812dd
Samuel gonçalves <goncalvesamuel99@gmail.com>
2021-06-25 15:44:57 +0100

    orientação objetos

---
+++ Objeto-js/OO.js
@@ -0,0 +1,24 @@
+// CÓDIGO NÃO EXECUTAVEL!!!
+
+// Procedural -- funções que manipulavam dados
+processamento (valor1, valor2, valor3)
+
+//OO - orientacao a objetos -- temos dados e dentro dos dados temos as funções
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+objeto.processamento() //a função processamento pertence ao objeto
+
+/*
+Principios Importantes:
+    1. - Abstrção
+    2. - encapsulamento -- ter os detalhes escondidos e mostrar apenas para quem precisa de os usar
+    3. - herança (prototype)
+    4. - polimorfismo - apartir do conceito generico, é possível criar varios objetos
+*/
