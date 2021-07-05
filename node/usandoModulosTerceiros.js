64d57067efedf7716ab644b4a50277448a45e8b8
Samuel gonçalves <goncalvesamuel99@gmail.com>
2021-07-05 13:44:34 +0100

    node js

---
+++ node/usandoModulosTerceiros.js
@@ -0,0 +1,2 @@
+const _ = require("lodash"); //* o underline é uma referencia à biblioteca lodash
+setInterval(() => console.log(_.random(1, 1000)), 2000); //! de 2 em 2 segundos vai gerer um numero random
