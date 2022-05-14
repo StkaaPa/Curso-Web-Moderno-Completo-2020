8fd576028a2bf0799494c94899b767cdf1d8c439
Samuel gonçalves <goncalvesamuel99@gmail.com>
2022-05-14 11:54:29 +0100

    webpack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,6 @@
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'development', //! caso nao coloque nenhum mode, é assumido o valor "production" como padrão
+  entry: './src/principal.js'
+}
