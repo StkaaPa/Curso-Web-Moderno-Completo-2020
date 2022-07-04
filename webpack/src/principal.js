c151125c1b6b77bf2dbec2d942cba56b9a330656
Samuel gonçalves <goncalvesamuel99@gmail.com>
2022-07-04 16:03:06 +0100

    removed

--- webpack/src/principal.js
+++
@@ -1,5 +0,0 @@
-// const Pessoa = require ('./pessoa')
-import Pessoa from './pessoa'
-
-const atendente = new Pessoa
-console.log(atendente.cumprimentar())
--- webpack/webpack.config.js
+++
@@ -1,6 +0,0 @@
-const webpack = require('webpack')
-
-module.exports = {
-  mode: 'development', //! caso nao coloque nenhum mode, é assumido o valor "production" como padrão
-  entry: './src/principal.js'
-}
